db95d86281a7f52232b24519bb442789cf394314
niraja426 <nrjshk@gmail.com>
2019-12-27 01:16:30 +0100

    Updates

---
+++ precache-manifest.fb7ef64706da62c9d73bfeae5c7d0c37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc44b539d003f7a109a81ca1ea400cf8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fa997541741970c54564",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c5b3fe1de5b0d90aeaee",
+    "url": "/react-gh-pages/static/js/2.5043b63b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.5043b63b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa997541741970c54564",
+    "url": "/react-gh-pages/static/js/main.4f35d096.chunk.js"
+  },
+  {
+    "revision": "3b80abee520c6ecf41cb",
+    "url": "/react-gh-pages/static/js/runtime-main.4dcaa514.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
